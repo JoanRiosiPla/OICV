6b9683996d45ac3ef40013de4c7cf6e546701a84
Joan Rios i Pla <104719628+JoanRiosiPla@users.noreply.github.com>
2024-01-16 11:09:48 +0000

    swdfasfsdf

---
+++ important.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <algorithm>
+
+#include <random>
+#include <time.h>
+
+using namespace std;
+
+vector<int> n, k;
+n.push_back(13);
+sort(n.begin(), n.end(), &sorter);
+bool sorter(int n, int p) {
+    return n < p;
+}
+
+cin >> c;
+cout << "Hi" << 12;
+
+string str = "Hi";
+str.size()
+
+srand(time(NULL)); // Start rand
+rand() % 10000
