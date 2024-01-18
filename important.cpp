60fa073ad08a87d5b01e15fe1b02f67ec093a1e9
Joan Rios i Pla <104719628+JoanRiosiPla@users.noreply.github.com>
2024-01-18 10:43:39 +0000

    important done

--- important.cpp
+++ important.cpp
@@ -8,18 +8,50 @@
 
 using namespace std;
 
-vector<int> n, k;
-n.push_back(13);
-sort(n.begin(), n.end(), &sorter);
 bool sorter(int n, int p) {
     return n < p;
 }
 
-cin >> c;
-cout << "Hi" << 12;
+class med
+{
+    public:
+        int volume = 0, value = 0, min = 0;
+        float relativeValue = 0;
+        float calcRelativeValue() {
+            this->relativeValue = (float)this->value / (float)this->volume;
+            return this->relativeValue;
+        }
+};
 
-string str = "Hi";
-str.size()
+struct player
+{
+    int position = 0;
+    int cards = 0;
+};
 
-srand(time(NULL)); // Start rand
-rand() % 10000
+int main(void) {
+    vector<int> n, k;
+    vector<int> q(10, 0); // Length, Value
+    vector<int> board[10][10];
+
+    n.push_back(13);
+    n.insert(n.begin(), -12);
+    sort(n.begin(), n.end(), &sorter);
+
+    n.pop_back();
+    n.erase(n.begin());
+
+    int c;
+    cout << "Input a number: ";
+    cin >> c;
+    cout << "Hi, the number is" << c << endl;
+
+    string str = "Hi";
+    str.size();
+
+    srand(time(NULL)); // Start rand
+    rand() % 10000;
+    return 0;
+}
+
+
